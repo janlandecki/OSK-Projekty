c8b54d57b1ff896b37e4d7e9d49ecfbf3ad644ce
Jakub Sawicki <kuba_sawicki@onet.pl>
2025-04-07 12:43:44 +0200

    Dodanie zegara i zmiany skórkui

--- witam/Form1.Designer.cs
+++ witam/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.butDivide = new System.Windows.Forms.Button();
@@ -38,15 +39,22 @@
             this.textVar = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.labelRes = new System.Windows.Forms.Label();
+            this.panelClock = new System.Windows.Forms.Panel();
+            this.labelDigital = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(17, 28);
+            this.label1.Location = new System.Drawing.Point(23, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(145, 25);
+            this.label1.Size = new System.Drawing.Size(184, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Wpisz wartość:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -56,9 +64,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(89, 69);
+            this.label2.Location = new System.Drawing.Point(119, 85);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 25);
+            this.label2.Size = new System.Drawing.Size(90, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Wynik:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -66,9 +75,10 @@
             // butDivide
             // 
             this.butDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.butDivide.Location = new System.Drawing.Point(298, 108);
+            this.butDivide.Location = new System.Drawing.Point(397, 133);
+            this.butDivide.Margin = new System.Windows.Forms.Padding(4);
             this.butDivide.Name = "butDivide";
-            this.butDivide.Size = new System.Drawing.Size(50, 50);
+            this.butDivide.Size = new System.Drawing.Size(67, 62);
             this.butDivide.TabIndex = 5;
             this.butDivide.Text = ":";
             this.butDivide.UseVisualStyleBackColor = true;
@@ -77,9 +87,10 @@
             // butMultiply
             // 
             this.butMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.butMultiply.Location = new System.Drawing.Point(203, 108);
+            this.butMultiply.Location = new System.Drawing.Point(271, 133);
+            this.butMultiply.Margin = new System.Windows.Forms.Padding(4);
             this.butMultiply.Name = "butMultiply";
-            this.butMultiply.Size = new System.Drawing.Size(50, 50);
+            this.butMultiply.Size = new System.Drawing.Size(67, 62);
             this.butMultiply.TabIndex = 6;
             this.butMultiply.Text = "*";
             this.butMultiply.UseVisualStyleBackColor = true;
@@ -88,9 +99,10 @@
             // butMinus
             // 
             this.butMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.butMinus.Location = new System.Drawing.Point(112, 108);
+            this.butMinus.Location = new System.Drawing.Point(149, 133);
+            this.butMinus.Margin = new System.Windows.Forms.Padding(4);
             this.butMinus.Name = "butMinus";
-            this.butMinus.Size = new System.Drawing.Size(50, 50);
+            this.butMinus.Size = new System.Drawing.Size(67, 62);
             this.butMinus.TabIndex = 7;
             this.butMinus.Text = "-";
             this.butMinus.UseVisualStyleBackColor = true;
@@ -99,9 +111,10 @@
             // butEquals
             // 
             this.butEquals.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.butEquals.Location = new System.Drawing.Point(399, 108);
+            this.butEquals.Location = new System.Drawing.Point(532, 133);
+            this.butEquals.Margin = new System.Windows.Forms.Padding(4);
             this.butEquals.Name = "butEquals";
-            this.butEquals.Size = new System.Drawing.Size(50, 50);
+            this.butEquals.Size = new System.Drawing.Size(67, 62);
             this.butEquals.TabIndex = 8;
             this.butEquals.Text = "=";
             this.butEquals.UseVisualStyleBackColor = true;
@@ -110,9 +123,10 @@
             // butPlus
             // 
             this.butPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.butPlus.Location = new System.Drawing.Point(22, 108);
+            this.butPlus.Location = new System.Drawing.Point(29, 133);
+            this.butPlus.Margin = new System.Windows.Forms.Padding(4);
             this.butPlus.Name = "butPlus";
-            this.butPlus.Size = new System.Drawing.Size(50, 50);
+            this.butPlus.Size = new System.Drawing.Size(67, 62);
             this.butPlus.TabIndex = 10;
             this.butPlus.Text = "+";
             this.butPlus.UseVisualStyleBackColor = true;
@@ -121,34 +135,98 @@
             // textVar
             // 
             this.textVar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textVar.Location = new System.Drawing.Point(168, 25);
+            this.textVar.Location = new System.Drawing.Point(224, 31);
+            this.textVar.Margin = new System.Windows.Forms.Padding(4);
             this.textVar.Name = "textVar";
-            this.textVar.Size = new System.Drawing.Size(281, 30);
+            this.textVar.Size = new System.Drawing.Size(373, 36);
             this.textVar.TabIndex = 11;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(163, 69);
+            this.label3.Location = new System.Drawing.Point(217, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.Size = new System.Drawing.Size(0, 29);
             this.label3.TabIndex = 12;
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // labelRes
             // 
             this.labelRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.labelRes.Location = new System.Drawing.Point(163, 69);
+            this.labelRes.Location = new System.Drawing.Point(217, 85);
+            this.labelRes.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelRes.Name = "labelRes";
-            this.labelRes.Size = new System.Drawing.Size(286, 25);
+            this.labelRes.Size = new System.Drawing.Size(381, 31);
             this.labelRes.TabIndex = 13;
+            // 
+            // panelClock
+            // 
+            this.panelClock.Location = new System.Drawing.Point(271, 287);
+            this.panelClock.Name = "panelClock";
+            this.panelClock.Size = new System.Drawing.Size(369, 302);
+            this.panelClock.TabIndex = 14;
+            this.panelClock.Paint += new System.Windows.Forms.PaintEventHandler(this.panelClock_Paint);
+            // 
+            // labelDigital
+            // 
+            this.labelDigital.AutoSize = true;
+            this.labelDigital.Location = new System.Drawing.Point(404, 429);
+            this.labelDigital.Name = "labelDigital";
+            this.labelDigital.Size = new System.Drawing.Size(93, 16);
+            this.labelDigital.TabIndex = 15;
+            this.labelDigital.Text = "Zegar Cyfrowy";
+            this.labelDigital.Click += new System.EventHandler(this.labelDigital_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(361, 646);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(134, 20);
+            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Zegar Analogowy";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(361, 683);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(114, 20);
+            this.radioButton2.TabIndex = 17;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Zegar Cyfrowy";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Location = new System.Drawing.Point(746, 49);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(156, 111);
+            this.buttonColor.TabIndex = 18;
+            this.buttonColor.Text = "Zmiana koloru skórki";
+            this.buttonColor.UseVisualStyleBackColor = true;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1039, 622);
+            this.ClientSize = new System.Drawing.Size(979, 766);
+            this.Controls.Add(this.buttonColor);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.labelDigital);
+            this.Controls.Add(this.panelClock);
             this.Controls.Add(this.labelRes);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textVar);
@@ -159,6 +237,7 @@
             this.Controls.Add(this.butDivide);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Kalkulator";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -179,6 +258,12 @@
         private System.Windows.Forms.TextBox textVar;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label labelRes;
+        private System.Windows.Forms.Panel panelClock;
+        private System.Windows.Forms.Label labelDigital;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- witam/Form1.cs
+++ witam/Form1.cs
@@ -12,9 +12,20 @@ namespace witam
 {
     public partial class Form1 : Form
     {
+        bool isAnalog = true; // Określa czy zegar analogowy/cyfrowy
+        Color skinColor = Color.LightBlue; // Domyślny kolor skórki
         public Form1()
         {
             InitializeComponent();
+
+            // Konfiguracja timera do zegarów
+            timer1.Interval = 1000; // 1 sekunda
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+
+            // Ustawienia początkowe widoczności zegarów
+            panelClock.Visible = isAnalog;
+            labelDigital.Visible = !isAnalog;
         }
         enum Op { PLUS, MINUS, MULTIPLY, DIVIDE };
         Op operation;
@@ -46,6 +57,91 @@ namespace witam
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                isAnalog = true;
+                panelClock.Visible = true;
+                labelDigital.Visible = false;
+            }
+        }
+
+        private void DrawHand(Graphics g, int cx, int cy, double angle, float length, int width, Color? color = null)
+        {
+            double rad = (Math.PI / 180) * (angle - 90);
+            float x = cx + (float)(length * Math.Cos(rad));
+            float y = cy + (float)(length * Math.Sin(rad));
+            using (Pen pen = new Pen(color ?? Color.Black, width))
+            {
+                g.DrawLine(pen, cx, cy, x, y);
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isAnalog)
+            {
+                panelClock.Invalidate(); // wywołuje zdarzenie Paint dla analogowego zegara
+            }
+            else
+            {
+                labelDigital.Text = DateTime.Now.ToLongTimeString(); // zaktualizowanie godziny zegara cyfrowego
+            }
+        }
+
+        private void panelClock_Paint(object sender, PaintEventArgs e)
+        {
+            if (!isAnalog) return;
+
+            DateTime now = DateTime.Now;
+            int centerX = panelClock.Width / 2;
+            int centerY = panelClock.Height / 2;
+            int radius = Math.Min(centerX, centerY) - 10;
+
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            // Rysowanie obwodu zegara
+            e.Graphics.DrawEllipse(Pens.Black, centerX - radius, centerY - radius, radius * 2, radius * 2);
+
+            // Rysowanie wskazówki godzinowej
+            double hourAngle = (now.Hour % 12 + now.Minute / 60.0) * 30; 
+            DrawHand(e.Graphics, centerX, centerY, hourAngle, radius * 0.5f, 6);
+
+            // Rysowanie wskazówki minutowej
+            double minuteAngle = (now.Minute + now.Second / 60.0) * 6;
+            DrawHand(e.Graphics, centerX, centerY, minuteAngle, radius * 0.7f, 4);
+
+            // Rysowanie wskazówki sekundowej
+            double secondAngle = now.Second * 6; 
+            DrawHand(e.Graphics, centerX, centerY, secondAngle, radius * 0.9f, 2, Color.Red);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                isAnalog = false;
+                panelClock.Visible = false;
+                labelDigital.Visible = true;
+            }
+        }
+
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog dlg = new ColorDialog())
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    skinColor = dlg.Color;
+                    this.BackColor = skinColor;
+                }
+            }
+        }
+
+        private void labelDigital_Click(object sender, EventArgs e)
         {
 
         }
